343181142053a13fb43ec77f2789774ead6b4203
Malcolm Lashley <mlashley@rbbn.com>
2021-08-22 14:04:36 +0000

    Malc's circlci

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,7 +4,7 @@ set -eux
 SRCROOT="$(cd "$(dirname "$0")/.." && pwd)"
 GIT_PUSH=${GIT_PUSH:-false}
 
-rm -rf $SRCROOT/output && git clone -b gh-pages git@github.com:openstack-exporter/helm-charts.git $SRCROOT/output
+rm -rf $SRCROOT/output && git clone -b gh-pages git@github.com:mlashley/helm-charts.git $SRCROOT/output
 
 for dir in $(find $SRCROOT/charts -mindepth 1 -maxdepth 1 -type d); do
     echo "Processing $dir"
@@ -17,5 +17,5 @@ cd $SRCROOT/output && helm repo index .
 cd $SRCROOT/output && git status
 
 if [ "$GIT_PUSH" == "true" ]; then
-    cd $SRCROOT/output && git add . && git commit -m "Publish charts" && git push git@github.com:openstack-exporter/helm-charts.git gh-pages
+    cd $SRCROOT/output && git add . && git commit -m "Publish charts" && git push git@github.com:mlashley/helm-charts.git gh-pages
 fi
